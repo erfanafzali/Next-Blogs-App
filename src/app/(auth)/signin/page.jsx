ebdc276d2a04b817eb91f0f5dcdc72a0c7e35ad2
Erfan Afzali <developer.invoker@gmail.com>
2024-09-16 17:57:16 +0330

    work on signin feature

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,13 +1,45 @@
-"use client"
+"use client";
 
+import Button from "@/components/ui/Button";
 import RHFTextField from "@/components/ui/RHFTextField";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signupApi } from "@/services/authService";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+const schema = yup.object({
+  name: yup
+    .string()
+    .min(5, "نام و نام خانوادگی نامعتبر است")
+    .max(30)
+    .required("نام و نام خانوادگی الزامی است"),
+  email: yup.string().email("ایمیل نامعتبر است").required("ایمیل الزامی است"),
+  password: yup.string().required("رمز عبور الزامی است"),
+});
 
 function SignIn() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (value) => {
-    console.log(value);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+  const onSubmit = async (value) => {
+    try {
+      const { user, message } = await signupApi(value);
+      router.push("/");
+
+      toast.success(message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
   };
+
   return (
     <div className="w-full">
       <h1></h1>
@@ -16,12 +48,16 @@ function SignIn() {
           name="name"
           label="نام و نام خانوادگی"
           register={register}
+          isRequired
+          errors={errors}
         />
         <RHFTextField
           name="email"
           label="ایمیل"
           register={register}
           dir="ltr"
+          isRequired
+          errors={errors}
         />
         <RHFTextField
           name="password"
@@ -29,8 +65,16 @@ function SignIn() {
           register={register}
           dir="ltr"
           type="password"
+          isRequired
+          errors={errors}
         />
-        <button className="btn btn--primary w-full relative top-5 text-xl font-bold text-secondary-0">تایید</button>
+        <Button
+          type="submit"
+          variant="primary"
+          className="w-full relative top-5 font-bold text-lg md:text-xl text-secondary-0"
+        >
+          تایید
+        </Button>
       </form>
     </div>
   );
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import vazirFont from "@/constants/localFonts";
 import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 // meta tags for seo
 export const metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans  min-h-screen`}>
+        <Toaster/>
         <Header />
         <div className="container xl:max-w-screen-xl">{children}</div>
       </body>
